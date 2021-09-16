66151a183c8715a7fbc2175748bf46993bb8eceb
eshmakar <59125020+eshmakar@users.noreply.github.com>
2021-09-16 23:02:13 +0300

    @Component

--- src/spring_intro/Cat.java
+++ src/spring_intro/Cat.java
@@ -1,5 +1,9 @@
 package spring_intro;
 
+import org.springframework.stereotype.Component;
+
+//это аннотация указывается для автоматического создания бинов из класса
+@Component
 public class Cat implements Pet {
     public Cat() {
         System.out.println("Cat bean is created");
