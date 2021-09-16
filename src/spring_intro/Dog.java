99c912b11f3144620562b4c1b9528d873e0749fe
eshmakar <59125020+eshmakar@users.noreply.github.com>
2021-09-16 22:29:08 +0300

    init and destroy methods

--- src/spring_intro/Dog.java
+++ src/spring_intro/Dog.java
@@ -2,11 +2,18 @@ package spring_intro;
 
 public class Dog implements Pet {
     public Dog() {
-        System.out.println("Dpg bean is created");
+        System.out.println("Dog bean is created (Constructor)");
     }
 
     @Override
     public void say() {
         System.out.println("How");
+    }
+
+    void init(){//этот метод будет выполняться сразу после создания бина (если scope - singleton)
+        System.out.println("This is init method");
+    }
+    void destrooy(){//этот метод будет выполняться после выполенения всех работ, после закрытия context(если scope - singleton)
+        System.out.println("This is destroy method");
     }
 }
--- src/spring_intro/Test3.java
+++ src/spring_intro/Test3.java
@@ -3,13 +3,13 @@ package spring_intro;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test3 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");//чтение конфигурационного файла
         Person person = context.getBean("myPerson", Person.class);//получение бинов
         person.callYourPet();//вызов методов
         System.out.println(person.getSurName());
         System.out.println(person.getAge());
-
         context.close();
+
     }
 }
