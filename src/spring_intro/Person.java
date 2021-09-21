2cbc5f9141f175775d136a6ae335ba59d74b9f9a
eshmakar <59125020+eshmakar@users.noreply.github.com>
2021-09-21 23:15:23 +0300

    @Qualifier("cat") // @Qualifier - указываем, какой именно бин будем использовать

--- src/spring_intro/Person.java
+++ src/spring_intro/Person.java
@@ -5,24 +5,25 @@ import org.springframework.stereotype.Component;
 
 @Component("myPerson")
 public class Person {
-    private final Pet pet;
+    @Autowired
+    private  Pet pet;
 
     //чтобы связывать объекты между собой. В этом примере автоматический создается бин myCat от типа Pet и добавляется сюда в конструктор
-    @Autowired
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+
+
+    public Person() {
         System.out.println("Person bean is created");
-        this.pet = pet;
     }
 
 
-////    public Person() {
-////        System.out.println("Person bean is created");
-////    }
-//
-//    public void setPet(Pet pet) {//если используется класс без конструктора, тогда можно все сделать через сеттеры
-//        System.out.println("Class Person method setPet");
-//        this.pet = pet;
-//    }
+    public void setPet(Pet pet) {
+        System.out.println("Class Person method setPet");
+        this.pet = pet;
+    }
 
     public void callYourPet() {
         System.out.println("Hello, my lovely pet");
